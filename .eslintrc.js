7b497ac3590d82b42564c653744ed0f585316b79
Alexey <Alexey@mail.ru>
2024-03-24 08:40:17 +0300

    storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,7 @@ module.exports = {
                 markupOnly: true,
                 ignoreAttribute: ['data-testId'],
             }],
-        'max-len': ['error', { ignoreComments: true }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,9 +1,8 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { buildCssLoader } from './loaders/buildCssLoader';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-    // для работы с svg изображениями //
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
@@ -21,7 +20,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
                         'i18next-extract',
                         {
                             locales: ['ru', 'en'],
-                            keyAsDefaultValue: true, // в качестве значения подставляет ключ
+                            keyAsDefaultValue: true,
                         },
                     ],
                 ],
@@ -29,34 +28,13 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         },
     };
 
-    // для работы со стилями //
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from JS strings
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')), // чтобы генерить названия классов только для module
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]',
-                    },
-                },
-            },
-            // Compiles Sass to CSS
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoader(isDev);
 
-    // Если не используем тайпскрипт - нужен babel-loader //
+    // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
-        test: /\.tsx?$/, // регулярка по которой мы находим файлы, которые необходимо пропустить через лоудер //
-        use: 'ts-loader', // loader, который для этих файлов необходимо использовать //
-        exclude: /node_modules/, // исключаем то, что обрабатывать не нужно //
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
     };
 
     const fileLoader = {
