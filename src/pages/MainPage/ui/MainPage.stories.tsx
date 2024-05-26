4a3dde5f40083eed009b382ac1f3b2af57589aa6
Alexey <Alexey@mail.ru>
2024-05-26 22:34:50 +0300

    ya_vernulsa

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import MainPage from 'pages/MainPage/ui/MainPage';
+import MainPage from './MainPage';
 
 export default {
     title: 'pages/MainPage',
@@ -12,8 +12,7 @@ export default {
     },
 } as ComponentMeta<typeof MainPage>;
 
-// @ts-ignore
-const Template: ComponentStory<typeof MainPage> = (args) => <MainPage {...args} />;
+const Template: ComponentStory<typeof MainPage> = () => <MainPage />;
 
 export const Normal = Template.bind({});
 Normal.args = {};
